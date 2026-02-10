a08a7bbbe20ef955719d23cde3ef91a9f918731f
Aakash Prajapati <aakashprajapati@Aakashs-MacBook-Pro.local>
2026-02-10 17:21:06 +0530

    create-flask-application-and-get-data-from-user-and-create-predict-pipeline

---
+++ src/ml_project/pipeline/predection_pipeline.py
@@ -0,0 +1,62 @@
+import sys
+
+import pandas as pd
+from src.ml_project.exception import CustomException
+from src.ml_project.logger import logging
+from src.ml_project.util import load_object
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self, features):
+        try:
+            model_path = "artifacts/model.pkl"
+            preprocessor_path = "artifacts/preprocessor.pkl"
+
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+            return preds
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+class CustomData:
+    def __init__(
+        self,
+        gender: str,
+        ethnicity: str,
+        parental_level_of_education: str,
+        lunch: str,
+        test_preparation_course: str,
+        writing_score: int,
+        reading_score: int,
+    ):
+        # This is the constructor of the class CustomData
+        # which will take the input from the user and store it in the instance variables
+        self.gender = gender
+        self.ethnicity = ethnicity
+        self.parental_level_of_education = parental_level_of_education
+        self.lunch = lunch
+        self.test_preparation_course = test_preparation_course
+        self.writing_score = writing_score
+        self.reading_score = reading_score
+
+    def get_data_as_data_frame(self):
+        try:
+            custom_data_input_dict = {
+                "gender": [self.gender],
+                "race_ethnicity": [self.ethnicity],
+                "parental_level_of_education": [self.parental_level_of_education],
+                "lunch": [self.lunch],
+                "test_preparation_course": [self.test_preparation_course],
+                "writing_score": [self.writing_score],
+                "reading_score": [self.reading_score],
+            }
+            return pd.DataFrame(custom_data_input_dict)
+        except Exception as e:
+            raise CustomException(e, sys)
