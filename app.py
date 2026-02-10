6afaa5da0c4ea46a820f5d24a5cd78f3571435b5
Aakash Prajapati <aakashprajapati@Aakashs-MacBook-Pro.local>
2026-02-10 11:54:14 +0530

    add-data-transformation-file-and-create-pickle-data-after-perform-data-transformation

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from src.ml_project.logger import logging
 from src.ml_project.exception import CustomException
 import sys
 from src.ml_project.components.data_ingestion import DataIngestion
+from src.ml_project.components.data_transformation import DataTransformation
 
 
 if __name__ == "__main__":
@@ -10,7 +11,13 @@ if __name__ == "__main__":
     try:
         # a = 1 / 0
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+
+        # Call Data Transformation
+        data_transformation = DataTransformation()
+        data_transformation.initiate_data_transformation(
+            train_data_path, test_data_path
+        )
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e, sys)
