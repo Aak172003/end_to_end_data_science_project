18132c9b170c6e9b0559e5a33d908b188071fd08
Aakash Prajapati <aakashprajapati@Aakashs-MacBook-Pro.local>
2026-02-11 11:34:17 +0530

    made-debug-false

--- application.py
+++ application.py
@@ -37,10 +37,9 @@ def predict_datapoint():
 
         predict_pipeline = PredictPipeline()
         results = predict_pipeline.predict(pred_df)
-        print("preds ::::::::::::::::: ", results)
 
         return render_template("home.html", results=results[0])
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5001)
+    app.run(host="0.0.0.0", port=5001)
